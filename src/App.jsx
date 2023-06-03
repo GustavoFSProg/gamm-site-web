c5cb8b89a60ed7d2d974f8cfac3350aa50eba420
GFSprog40 <gustavo.prog40@gmail.com>
2023-06-03 14:59:51 -0300

    commiting]

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header/Header'
-import { Container } from './style-app'
+import { Container, ContainerText, ContainerTraducao, ContainerTitle } from './style-app'
 
 function App() {
 
@@ -7,6 +7,25 @@ function App() {
     <>
       <Container >
         <Header />
+        <ContainerTitle >
+          <ContainerTraducao >
+            <ContainerText >
+
+              Tradução Juramentada
+              Tradutora Pública e Intérprete
+              Comercial para o Idioma Alemão
+              Alemão / Português
+
+            </ContainerText >
+
+
+            <h1 >
+              TEsting
+            </h1>
+          </ContainerTraducao >
+
+        </ContainerTitle>
+
       </Container>
 
     </>
--- src/style-app.jsx
+++ src/style-app.jsx
@@ -7,7 +7,46 @@ export const Container = styled.div`
  width: 100vw;
  height: 100vh;
  background: url(${foto1}) no-repeat;
- opacity: 76%;
+ opacity: 80%;
+`
+
+export const ContainerTraducao = styled.div`
+ display: flex; 
+ flex-direction: row;
+ width: 75%;
+ /* height: 26rem; */
+ /* opacity: 52%; */
+ /* background: #204060; */
+ /* margin-top: 130px; */
+ justify-content: space-around;
+ align-items: center;
+
+
+`
+
+export const ContainerTitle = styled.div`
+ display: flex; 
+ flex-direction: column;
+ width: 100%;
+ height: 26rem;
+ opacity: 72%;
+ background: #132639;
+ margin-top: 130px;
+ justify-content: center;
+ align-items: center;
+
+
+`
+
+export const ContainerText = styled.div`
+ display: flex; 
+ width: 25%;
+ height: 26rem;
+ justify-content: center;
+ align-items: center;
+ color: white;
+ opacity: 180%;
+ font-size: 22px;
 
 
 `
