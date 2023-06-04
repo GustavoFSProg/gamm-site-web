78f2e5e71be19c6e3d5f9e990d63cb4e40e9f9bc
GFSprog40 <gustavo.prog40@gmail.com>
2023-06-04 12:35:58 -0300

    fazendo

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import Header from './components/Header/Header'
 import { Container, Button, Input, Span, ContainerText, ContainerTraducao, ContainerTitle } from './style-app'
 
@@ -18,22 +19,30 @@ function App() {
               Alemão / Português
 
             </ContainerText >
+            <h1 >
+              Solicite um orçamento
+            </h1>
 
             <form style={{ display: 'flex', flexDirection: 'column' }}>
-              <Input type="text" placeholder="Nome" />
+              {/* <Input type="text" placeholder="Nome" />
               <Input type="text" placeholder="Email" />
               <Input type="text" placeholder="Telefone" />
               <Button >
                 Enviar
-              </ Button >
+              </ Button > */}
+              {/* <a href="http://mailto:gustavosohne38@gmail.com?subject=Orçamento">Enviar email</a> */}
+              {/* <a href="http://mailto:gustavosohne38@gmail.com">EFERER </a> */}
 
+              <a style={{ display: 'flex', textDecoration: 'none', color: 'yellow', fontSize: '20px' }} href="http://mailto:gustavosohne38@gmail.com">Enviar email</a>
+
+              {/* <a href=”http://mailto:gustavosohne38@gmail.com?subject=My Subject Line” >ENTRE EM CONTATO</a> */}
             </form>
 
           </ContainerTraducao >
 
         </ContainerTitle>
 
-      </Container>
+      </Container >
 
     </>
   )
